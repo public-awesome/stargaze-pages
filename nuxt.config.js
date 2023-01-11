2235db84ea0f7c8f9bfcf277771cef04f4e75d37
Nik Rykov <rykov.nikas@gmail.com>
2023-01-11 18:27:25 +0000

    Inter font fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
     modules: ['@nuxtjs/tailwindcss'],
     mode: "universal",
-
+    css: ["@/assets/css/main.css"],
     app: {
         head: {
             htmlAttrs: { "data-theme": "luxury", }
