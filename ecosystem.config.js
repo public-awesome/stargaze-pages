142f83af8f48ee5aa21c4b2996ae1d6b1b575714
Mykyta Rykov <rykov.nikas@gmail.com>
2023-01-30 05:42:22 +0000

    first deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,5 +10,17 @@ module.exports = {
                 "HOST": "0.0.0.0"
             }
         }
-    ]
+    ],
+    deploy: {
+        production: {
+            user: 'root',
+            host: process.env.SERVER_IP,
+            key: 'deploy.key',
+            ref: 'origin/master',
+            repo: 'https://github.com/public-awesome/stargaze-pages',
+            path: '/root/stargaze-pages',
+            'post-deploy':
+                'yarn install && yarn build && pm2 reload ecosystem.config.js && pm2 save && git checkout yarn.lock',
+        },
+    },
 }
